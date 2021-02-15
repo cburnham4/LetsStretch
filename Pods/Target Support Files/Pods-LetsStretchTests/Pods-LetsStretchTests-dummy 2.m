f3614904c8a8aee166007d5891443d8a138a0ba5
Chase Burnham <cburnhamiv@gmail.com>
2021-02-15 14:38:09 -0700

    remove pod folder

--- Pods/Target Support Files/Pods-LetsStretchTests/Pods-LetsStretchTests-dummy 2.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LetsStretchTests : NSObject
-@end
-@implementation PodsDummy_Pods_LetsStretchTests
-@end
--- Pods/Target Support Files/Pods-LetsStretchUITests/Pods-LetsStretchUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LetsStretchUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LetsStretchUITests
-@end
